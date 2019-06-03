7b351486b26a818ce9be4b6409feeec36c1879a4
salma <salmaahhmed2@yahoo.com>
2019-06-03 10:09:15 +0200

    UI fixes
    improved UI for login and signup page

---
+++ CarSpareParts/app/src/main/java/com/example/carspareparts/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.carspareparts
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+//import com.parse.ParseInstallation
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        ParseInstallation.getCurrentInstallation().saveInBackground()
+
+    }
+}
