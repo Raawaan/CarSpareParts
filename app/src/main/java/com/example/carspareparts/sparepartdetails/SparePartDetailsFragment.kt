36a93b5184e1d7fbb5a706b20e8211d23ace4451
salma <salmaahhmed2@yahoo.com>
2019-06-06 15:38:19 +0200

    -finalized UI updates -merge local master with remote master

--- app/src/main/java/com/example/carspareparts/sparepartdetails/SparePartDetailsFragment.kt
+++ app/src/main/java/com/example/carspareparts/sparepartdetails/SparePartDetailsFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.carspareparts.R
 import com.example.carspareparts.sparepartproducts.SparePartDetails
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_spare_part_details.*
 import kotlinx.android.synthetic.main.app_bar_home.*
 
 class SparePartDetailsFragment : Fragment() {
@@ -26,6 +30,14 @@ class SparePartDetailsFragment : Fragment() {
         val sparePartDetails = arguments?.getParcelable<SparePartDetails>("sparePartDetails")
         sparePartDetails?.supplierName
         activity?.toolbar?.title="${sparePartDetails?.name} Details"
+        productNameTextView.text = sparePartDetails?.name
+
+        descriptionTextView.text = sparePartDetails?.description
+
+        supplierTextView.text = sparePartDetails?.supplierName
+
+        Picasso.get().load(sparePartDetails?.image).into(productImageView)
+
 
     }
 }
