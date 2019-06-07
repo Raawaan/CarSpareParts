e3c44ec2d4d229c79caedad116447601ad728401
Rawan Ali <rawaan_ali97@hotmail.com>
2019-06-07 14:35:14 +0200

    -comment some use used code

--- app/src/main/java/com/example/carspareparts/home/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/carspareparts/home/HomeFragmentViewModel.kt
@@ -7,15 +7,10 @@ import com.parse.ParseQuery
 
 class HomeFragmentViewModel : ViewModel() {
     private val sparePartTypeList = MutableLiveData<List<ParseObject>>()
-    private val set= mutableSetOf<String?>()
     fun getSparePartTypeList(){
-        val sparePartTypeQuery = ParseQuery<ParseObject>("spare_part")
-        sparePartTypeQuery.include("spare_part_type_id")
+        val sparePartTypeQuery = ParseQuery<ParseObject>("spare_part_type")
         sparePartTypeQuery.findInBackground { objects, e ->
             if (e==null){
-                objects.forEach {
-                    set.add(it.getString("spare_part_type_id"))
-                }
                 sparePartTypeList.postValue(objects)
             }
 //            else
@@ -26,3 +21,25 @@ class HomeFragmentViewModel : ViewModel() {
         return sparePartTypeList
     }
 }
+//
+//class HomeFragmentViewModel : ViewModel() {
+//    private val sparePartTypeList = MutableLiveData<List<ParseObject>>()
+//    private val set= mutableSetOf<String?>()
+//    fun getSparePartTypeList(){
+//        val sparePartTypeQuery = ParseQuery<ParseObject>("spare_part")
+//        sparePartTypeQuery.include("spare_part_type_id")
+//        sparePartTypeQuery.findInBackground { objects, e ->
+//            if (e==null){
+//                objects.forEach {
+//                    set.add(it.getString("spare_part_type_id"))
+//                }
+//                sparePartTypeList.postValue(objects)
+//            }
+////            else
+////                sparePartTypeList.postValue(e)
+//        }
+//    }
+//    fun getSparePartTypeLiveData(): MutableLiveData<List<ParseObject>> {
+//        return sparePartTypeList
+//    }
+//}
